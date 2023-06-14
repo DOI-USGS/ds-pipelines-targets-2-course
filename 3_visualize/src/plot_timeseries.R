b357a3ed77a495ad0e1e8e678cbe2fe18b99654b
Julie Padilla <jpadilla.usgs@gmail.com>
2023-06-14 16:09:56 -0700

    theme on new line
    Co-authored-by: Cee Nell <cnell@usgs.gov>

--- 3_visualize/src/plot_timeseries.R
+++ 3_visualize/src/plot_timeseries.R
@@ -3,7 +3,8 @@ plot_nwis_timeseries <- function(fileout, site_data_styled, width = 12, height =
   ggplot(data = site_data_styled, 
       aes(x = dateTime, y = water_temperature, color = station_name)
       ) +
-    geom_line() + theme_bw()
+    geom_line() + 
+    theme_bw()
   ggsave(fileout, width = width, height = height, units = units)
   
   return(fileout)
